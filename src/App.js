89d056b6fcd680fa3781ab461ddbf757d5035c71
pimook <pimookn@hotmail.com>
2022-11-01 09:32:20 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Link,Route,Routes}  from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import Home from "./Home"
 import ListProduct from "./ListProduct"
 import ListRegister from './backoffice/ListRegister'
@@ -8,29 +8,29 @@ import ListCustomer from 'backoffice/ListCustomer';
 function App() {
   return (
     <div className="Accusoft โปรแกรมธุรกิจ SME">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/listProduct">โปรแกรม</Link>
-          </li>
-          <li>
-            <Link to="/listRegister">การลงทะเบียน</Link>
-          </li>
-          <li>
-            <Link to="/listCustomer">Customer</Link>
-          </li>
-        </ul>
-      </nav>
-      <hr/>
-<Routes>
-  <Route path="/" element={<Home/>}></Route>
-  <Route path="/listProduct" element={<ListProduct/>}></Route>
-  <Route path="/listRegister" element={<ListRegister/>}></Route>  
-  <Route path="/listCustomer" element={<ListCustomer/>}></Route>  
-</Routes>
+      <header>
+        <h1 class="logo">Logo</h1>
+        <input type="checkbox" id="nav-toggle" class="nav-toggle" />
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/listProduct">โปรแกรม</Link></li>
+            <li><Link to="/listRegister">การลงทะเบียน</Link></li>
+            <li><Link to="/listCustomer">Customer</Link></li>
+          </ul>
+        </nav>
+        <label for="nav-toggle" class="nav-toggle-label">
+          <span></span>
+        </label>
+      </header>
+      <br/><br/><br/>
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/listProduct" element={<ListProduct />}></Route>
+        <Route path="/listRegister" element={<ListRegister />}></Route>
+        <Route path="/listCustomer" element={<ListCustomer />}></Route>
+      </Routes>
     </div>
   );
 }

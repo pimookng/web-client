1403e307b5b9d6a0926ae43352c9826e288599cd
pimook <pimookn@hotmail.com>
2022-11-09 16:06:16 +0700

    -

--- src/App.js
+++ src/App.js
@@ -9,25 +9,25 @@ import Product from 'backoffice/ListProduct'
 function App() {
   return (
     <div className="Accusoft โปรแกรมธุรกิจ SME">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/listProduct">โปรแกรม</Link>
-          </li>
-          <li>
-            <Link to="/listRegister">การลงทะเบียน</Link>
-          </li>
-          <li>
-            <Link to="/listCustomer">Customer</Link>
-          </li>
-          <li>
-            <Link to="backoffice/listProduct">Product</Link>
-          </li>
-        </ul>
-      </nav>
+      <header>
+        <h1 class="logo">Logo</h1>
+        <input type="checkbox" id="nav-toggle" class="nav-toggle" />
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/listProduct">โปรแกรม</Link></li>
+            <li><Link to="/listRegister">การลงทะเบียน</Link></li>
+            <li><Link to="/listCustomer">Customer</Link></li>
+            <li><Link to="/backoffice/listProduct">Product</Link></li>            
+          </ul>
+        </nav>
+        <label for="nav-toggle" class="nav-toggle-label">
+          <span></span>
+        </label>
+      </header>
+      <hr/>
+      <hr/>
+      <hr/>
       <hr/>
 <Routes>
   <Route path="/" element={<Home/>}></Route>
